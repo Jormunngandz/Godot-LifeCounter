a58356eae8a7b28f9673a1655be08eda51cab260
Dodo_laptorp <jormungand.dz@gmail.com>
2025-03-06 17:07:41 +0300

    Add start board scene

---
+++ scenes/board/board.gd
@@ -0,0 +1,21 @@
+extends PanelContainer
+@onready var option_class: PackedScene = preload("res://scenes/board_menu/panel_container.tscn")
+
+func _ready() -> void:
+	var players_layout = Global.selected_layout.instantiate()
+	if DisplayServer.screen_get_orientation() != players_layout.layout_orientation:
+		print('change screen orientation')
+		DisplayServer.screen_set_orientation(players_layout.layout_orientation)
+		get_viewport().size = Vector2(get_viewport().size.y, get_viewport().size.x)
+		var timer = get_tree().create_timer(1)
+		await timer.timeout
+	#
+	add_child(players_layout)
+	move_child(players_layout, 0)
+
+
+func _on_texture_rect_gui_input(event: InputEvent) -> void:
+	if event is InputEventScreenTouch:
+		if not event.is_pressed():
+			var options = option_class.instantiate()
+			add_child(options)
