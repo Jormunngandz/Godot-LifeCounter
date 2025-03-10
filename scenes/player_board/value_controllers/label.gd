f61d544bef48fa3200eb23520d00f56a84eb9fff
Dodo_laptorp <jormungand.dz@gmail.com>
2025-03-10 17:04:48 +0300

    add toggle functionality for control button

--- scenes/player_board/value_controllers/label.gd
+++ scenes/player_board/value_controllers/label.gd
@@ -1,9 +1,26 @@
-extends Label
+extends Button
 @export var change_value: int
 @onready var controlled_item
+var pressed_btn: bool = false
+var been_pressed: bool = false
 
 
-func _on_gui_input(event: InputEvent) -> void:
-	if event is InputEventScreenTouch:
-		if not event.is_pressed():
-			controlled_item.change_focus_value(change_value)
+func _on_button_up() -> void:
+	pressed_btn = false
+	if not been_pressed:
+		controlled_item.change_focus_value(change_value)
+	been_pressed = false
+	$Timer.wait_time = 1
+
+func _on_button_down() -> void:
+	pressed_btn = true
+	$Timer.start()
+
+
+func _on_timer_timeout() -> void:
+	if pressed_btn:
+		if been_pressed:
+			$Timer.wait_time = 0.5
+		been_pressed = true
+		controlled_item.change_focus_value(change_value * 10)
+		$Timer.start()
